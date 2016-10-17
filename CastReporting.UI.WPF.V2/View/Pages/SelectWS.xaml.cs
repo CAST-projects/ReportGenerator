2184b3d917c4a13be5941bf63725542bf4db0ce8
AEteve <a.eteve@castsoftware.com>
2016-10-17 16:25:19 +0200

    REPORTGEN-54
    Move to version 1.4.1.0 and use a PasswordBox for connection instead of
    textBox

--- CastReporting.UI.WPF.V2/View/Pages/SelectWS.xaml.cs
+++ CastReporting.UI.WPF.V2/View/Pages/SelectWS.xaml.cs
@@ -17,6 +17,7 @@ using System.Windows.Controls;
 using CastReporting.UI.WPF.Resources.Languages;
 using CastReporting.UI.WPF.ViewModel;
 using CastReporting.Domain;
+using System.Windows;
 
 namespace CastReporting.UI.WPF.View
 {
@@ -36,7 +37,6 @@ namespace CastReporting.UI.WPF.View
             DataContext = new SelectWSVM();
              
         }
-                 
-        
+
     }
 }
