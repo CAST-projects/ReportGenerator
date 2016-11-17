9c5a285c5dc0d27348a1c4fb8d661c365a44907e
AEteve <a.eteve@castsoftware.com>
2016-11-17 17:28:24 +0100

    REPORTGEN-7 : APPLICATION_RULE

--- CastReporting.Reporting/Block/Text/ApplicationRule.cs
+++ CastReporting.Reporting/Block/Text/ApplicationRule.cs
@@ -21,6 +21,7 @@ using CastReporting.Reporting.Builder.BlockProcessing;
 using CastReporting.Reporting.ReportingModel;
 using CastReporting.BLL.Computing;
 using CastReporting.Domain;
+using CastReporting.Reporting.Helper;
 
 namespace CastReporting.Reporting.Block.Text
 {
@@ -31,13 +32,22 @@ namespace CastReporting.Reporting.Block.Text
         protected override string Content(ReportData reportData, Dictionary<string, string> options)
         {
           
-            Int32? metricId = (options != null && options.ContainsKey("ID")) ? Convert.ToInt32(options["ID"]) : (Int32?)null;
+            int metricId = options.GetIntOption("ID", 0);
+            string _snapshot = options.GetOption("SNAPSHOT", "CURRENT");
            
             if (null != reportData &&
-                null != reportData.CurrentSnapshot && metricId.HasValue)
+                null != reportData.CurrentSnapshot && metricId != 0)
             {
-                double? result = BusinessCriteriaUtility.GetMetricValue(reportData.CurrentSnapshot, metricId.Value);
-                return result.HasValue ? result.Value.ToString("N2") : Constants.No_Value;
+                if (_snapshot == "PREVIOUS" && reportData.PreviousSnapshot != null)
+                {
+                    double? result = BusinessCriteriaUtility.GetMetricValue(reportData.PreviousSnapshot, metricId);
+                    return result.HasValue ? result.Value.ToString("N2") : Constants.No_Value;
+                }
+                else
+                {
+                    double? result = BusinessCriteriaUtility.GetMetricValue(reportData.CurrentSnapshot, metricId);
+                    return result.HasValue ? result.Value.ToString("N2") : Constants.No_Value;
+                }
             }
             return CastReporting.Domain.Constants.No_Value;
         }
