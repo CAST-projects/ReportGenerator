0af4744cccae44a38635d0e857c7c849e006ea02
AEteve <a.eteve@castsoftware.com>
2017-03-16 14:37:03 +0100

    REPORTGEN-234 : add comment

--- CastReporting.Reporting/Block/Graph/GenericGraph.cs
+++ CastReporting.Reporting/Block/Graph/GenericGraph.cs
@@ -3,12 +3,8 @@ using System.Collections.Generic;
 using CastReporting.Reporting.Atrributes;
 using CastReporting.Reporting.Builder.BlockProcessing;
 using CastReporting.Reporting.ReportingModel;
-using CastReporting.BLL.Computing;
-using CastReporting.Domain;
 using CastReporting.Reporting.Helper;
-using System.Linq;
-using CastReporting.Reporting.Languages;
-using CastReporting.BLL.Computing.DTO;
+
 
 namespace CastReporting.Reporting.Block.Graph
 {
@@ -17,6 +13,7 @@ namespace CastReporting.Reporting.Block.Graph
     {
         public override TableDefinition Content(ReportData reportData, Dictionary<string, string> options)
         {
+            // false for graph component
             return GenericContent.Content(reportData, options, false);
         }
     }
