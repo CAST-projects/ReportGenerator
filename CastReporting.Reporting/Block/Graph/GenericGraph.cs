ef5dc8cf52806b8a055777cad28ea82c4c2de89e
AEteve <a.eteve@castsoftware.com>
2017-03-16 17:06:33 +0100

    REPORTGEN-253 fix issue Health factors by Module for current snapshot is showing metrics on X-axis when there is single module

--- CastReporting.Reporting/Block/Graph/GenericGraph.cs
+++ CastReporting.Reporting/Block/Graph/GenericGraph.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using CastReporting.Reporting.Atrributes;
 using CastReporting.Reporting.Builder.BlockProcessing;
 using CastReporting.Reporting.ReportingModel;
