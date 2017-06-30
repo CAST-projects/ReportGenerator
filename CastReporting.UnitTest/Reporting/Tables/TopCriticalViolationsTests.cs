71dd8d747037f6c626e63228510b1e6fdd2e1e06
AEteve <a.eteve@castsoftware.com>
2017-06-30 15:04:16 +0200

    REPORTGEN-261 : add unit tests on table components

---
+++ CastReporting.UnitTest/Reporting/Tables/TopCriticalViolationsTests.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using CastReporting.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CastReporting.Reporting.ReportingModel;
+
+namespace CastReporting.UnitTest.Reporting.Tables
+{
+    [TestClass]
+    public class TopCriticalViolationsTests
+    {
+        [TestInitialize()]
+        public void Initialize()
+        {
+            TestUtility.SetCulture("en-US");
+        }
+
+        [TestMethod]
+        [DeploymentItem(@".\Data\CurrentBCTC.json", "Data")]
+        [DeploymentItem(@".\Data\PreviousBCTC.json", "Data")]
+        [DeploymentItem(@".\Data\cc60011.json", "Data")]
+        [DeploymentItem(@".\Data\cc60011previous.json", "Data")]
+        [DeploymentItem(@".\Data\nc60011.json", "Data")]
+        [DeploymentItem(@".\Data\nc60011previous.json", "Data")]
+        public void TestContent()
+        {
+            CastDate currentDate = new CastDate { Time = 1484953200000 };
+            CastDate previousDate = new CastDate { Time = 1484866800000 };
+            ReportData reportData = TestUtility.PrepareApplicationReportData("ReportGenerator",
+                null, @".\Data\CurrentBCTC.json", "AED/applications/3/snapshots/6", "PreVersion 1.5.0 sprint 2 shot 2", "V-1.5.0_Sprint 2_2", currentDate,
+                null, @".\Data\PreviousBCTC.json", "AED/applications/3/snapshots/3", "PreVersion 1.4.1 before release", "V-1.4.1", previousDate);
+            reportData = TestUtility.AddCriticalRuleViolations(reportData, 60011, @".\Data\cc60011.json", @".\Data\cc60011previous.json");
+            reportData = TestUtility.AddNonCriticalRuleViolations(reportData,60011, @".\Data\nc60011.json", @".\Data\nc60011previous.json");
+
+            var component = new CastReporting.Reporting.Block.Table.TopCriticalViolations();
+            Dictionary<string, string> config = new Dictionary<string, string>
+            {
+                {"PAR","60011" }
+            };
+            var table = component.Content(reportData, config);
+
+            var expectedData = new List<string>();
+            expectedData.AddRange(new List<string> { "Rule Name", "# Violations"});
+            expectedData.AddRange(new List<string> { "Avoid hiding static Methods", "63" });
+            expectedData.AddRange(new List<string> { "Suspicious similar method names or signatures in an inheritance tree", "13" });
+            expectedData.AddRange(new List<string> { "Avoid using untyped DataSet", "4" });
+            TestUtility.AssertTableContent(table, expectedData, 2, 4);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@".\Data\CurrentBCTC.json", "Data")]
+        [DeploymentItem(@".\Data\PreviousBCTC.json", "Data")]
+        [DeploymentItem(@".\Data\cc60011.json", "Data")]
+        [DeploymentItem(@".\Data\cc60011previous.json", "Data")]
+        [DeploymentItem(@".\Data\nc60011.json", "Data")]
+        [DeploymentItem(@".\Data\nc60011previous.json", "Data")]
+        public void TestLimitCount()
+        {
+            CastDate currentDate = new CastDate { Time = 1484953200000 };
+            CastDate previousDate = new CastDate { Time = 1484866800000 };
+            ReportData reportData = TestUtility.PrepareApplicationReportData("ReportGenerator",
+                null, @".\Data\CurrentBCTC.json", "AED/applications/3/snapshots/6", "PreVersion 1.5.0 sprint 2 shot 2", "V-1.5.0_Sprint 2_2", currentDate,
+                null, @".\Data\PreviousBCTC.json", "AED/applications/3/snapshots/3", "PreVersion 1.4.1 before release", "V-1.4.1", previousDate);
+            reportData = TestUtility.AddCriticalRuleViolations(reportData, 60011, @".\Data\cc60011.json", @".\Data\cc60011previous.json");
+            reportData = TestUtility.AddNonCriticalRuleViolations(reportData, 60011, @".\Data\nc60011.json", @".\Data\nc60011previous.json");
+
+            var component = new CastReporting.Reporting.Block.Table.TopCriticalViolations();
+            Dictionary<string, string> config = new Dictionary<string, string>
+            {
+                {"PAR","60011" },
+                {"COUNT","2" }
+            };
+            var table = component.Content(reportData, config);
+
+            var expectedData = new List<string>();
+            expectedData.AddRange(new List<string> { "Rule Name", "# Violations" });
+            expectedData.AddRange(new List<string> { "Avoid hiding static Methods", "63" });
+            expectedData.AddRange(new List<string> { "Suspicious similar method names or signatures in an inheritance tree", "13" });
+            TestUtility.AssertTableContent(table, expectedData, 2, 3);
+        }
+
+    }
+}
+
---
+++ CastReporting.UnitTest/Reporting/Tables/TopNonCriticalViolationsTests.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using CastReporting.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CastReporting.Reporting.ReportingModel;
+
+namespace CastReporting.UnitTest.Reporting.Tables
+{
+    [TestClass]
+    public class TopNonCriticalViolationsTests
+    {
+        [TestInitialize()]
+        public void Initialize()
+        {
+            TestUtility.SetCulture("en-US");
+        }
+
+        [TestMethod]
+        [DeploymentItem(@".\Data\CurrentBCTC.json", "Data")]
+        [DeploymentItem(@".\Data\PreviousBCTC.json", "Data")]
+        [DeploymentItem(@".\Data\cc60011.json", "Data")]
+        [DeploymentItem(@".\Data\cc60011previous.json", "Data")]
+        [DeploymentItem(@".\Data\nc60011.json", "Data")]
+        [DeploymentItem(@".\Data\nc60011previous.json", "Data")]
+        public void TestContent()
+        {
+            CastDate currentDate = new CastDate { Time = 1484953200000 };
+            CastDate previousDate = new CastDate { Time = 1484866800000 };
+            ReportData reportData = TestUtility.PrepareApplicationReportData("ReportGenerator",
+                null, @".\Data\CurrentBCTC.json", "AED/applications/3/snapshots/6", "PreVersion 1.5.0 sprint 2 shot 2", "V-1.5.0_Sprint 2_2", currentDate,
+                null, @".\Data\PreviousBCTC.json", "AED/applications/3/snapshots/3", "PreVersion 1.4.1 before release", "V-1.4.1", previousDate);
+            reportData = TestUtility.AddCriticalRuleViolations(reportData, 60011, @".\Data\cc60011.json", @".\Data\cc60011previous.json");
+            reportData = TestUtility.AddNonCriticalRuleViolations(reportData,60011, @".\Data\nc60011.json", @".\Data\nc60011previous.json");
+
+            var component = new CastReporting.Reporting.Block.Table.TopNonCriticalViolations();
+            Dictionary<string, string> config = new Dictionary<string, string>
+            {
+                {"PAR","60011" }
+            };
+            var table = component.Content(reportData, config);
+
+            var expectedData = new List<string>();
+            expectedData.AddRange(new List<string> { "Rule Name", "# Violations"});
+            expectedData.AddRange(new List<string> { "Avoid unreferenced Tables", "209" });
+            expectedData.AddRange(new List<string> { "Enumerations naming convention - case and character set control", "1" });
+            TestUtility.AssertTableContent(table, expectedData, 2, 3);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@".\Data\CurrentBCTC.json", "Data")]
+        [DeploymentItem(@".\Data\PreviousBCTC.json", "Data")]
+        [DeploymentItem(@".\Data\cc60011.json", "Data")]
+        [DeploymentItem(@".\Data\cc60011previous.json", "Data")]
+        [DeploymentItem(@".\Data\nc60011.json", "Data")]
+        [DeploymentItem(@".\Data\nc60011previous.json", "Data")]
+        public void TestLimitCount()
+        {
+            CastDate currentDate = new CastDate { Time = 1484953200000 };
+            CastDate previousDate = new CastDate { Time = 1484866800000 };
+            ReportData reportData = TestUtility.PrepareApplicationReportData("ReportGenerator",
+                null, @".\Data\CurrentBCTC.json", "AED/applications/3/snapshots/6", "PreVersion 1.5.0 sprint 2 shot 2", "V-1.5.0_Sprint 2_2", currentDate,
+                null, @".\Data\PreviousBCTC.json", "AED/applications/3/snapshots/3", "PreVersion 1.4.1 before release", "V-1.4.1", previousDate);
+            reportData = TestUtility.AddCriticalRuleViolations(reportData, 60011, @".\Data\cc60011.json", @".\Data\cc60011previous.json");
+            reportData = TestUtility.AddNonCriticalRuleViolations(reportData, 60011, @".\Data\nc60011.json", @".\Data\nc60011previous.json");
+
+            var component = new CastReporting.Reporting.Block.Table.TopNonCriticalViolations();
+            Dictionary<string, string> config = new Dictionary<string, string>
+            {
+                {"PAR","60011" },
+                {"COUNT","1" }
+            };
+            var table = component.Content(reportData, config);
+
+            var expectedData = new List<string>();
+            expectedData.AddRange(new List<string> { "Rule Name", "# Violations" });
+            expectedData.AddRange(new List<string> { "Avoid unreferenced Tables", "209" });
+            TestUtility.AssertTableContent(table, expectedData, 2, 2);
+        }
+
+    }
+}
+
