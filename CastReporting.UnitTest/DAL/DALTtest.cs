887ea9c4a65b6bcd688ad1dc1d23ebdf0f41b473
Aurore Eteve <a.eteve@castsoftware.com>
2018-07-31 07:23:42 +0200

    REPORTGEN-338 : avoid test failed when dashboard-Noc-170 is unavailable

--- CastReporting.UnitTest/DAL/DALTtest.cs
+++ CastReporting.UnitTest/DAL/DALTtest.cs
@@ -331,7 +331,11 @@ namespace CastReporting.UnitTest.DAL
 
             TestUtility.SetCulture("zh-CN");
             ICastRepsitory ccontext = new CastRepository(_connection3);
-            if (!ccontext.IsServiceValid()) return;
+            if (Environment.UserName != "abd")
+            {
+                if (ccontext == null) return;
+                if (!ccontext.IsServiceValid()) return;
+            }
             const string cdomainHref = "ADG83/quality-indicators/7126/snapshots/1";
             var result = ccontext.GetConfBusinessCriteria(cdomainHref);
             Assert.AreEqual("避免工件的已注释掉代码行/代码行的比率过高", result.Name);
